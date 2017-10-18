04546362df78769d59213d5eed931532f42346f4
Ben Obee <benobee@gmail.com>
2017-10-18 09:57:56 -0700

    grid change

--- src/modules/productListController.js
+++ src/modules/productListController.js
@@ -1,23 +1,35 @@
-import Vue from 'vue';
+import Vue from "vue";
 import axios from "axios";
 import { productList, productListImage, backToTopButton } from "../components/index";
+//import JSONData from "./data.json";
+
+const dev = false;
 
 const productListController = {
-    init(collection) {
+    init (collection) {
         const options = this.getShopOptions(collection);
 
-        const hasCachedData = sessionStorage.getItem("shop-data", true);
+        if (dev) {
 
-        if (hasCachedData === "true") {
-            const data = JSON.parse(sessionStorage.getItem("shop"));
+/*            const data = JSONData;
+        
+            this.renderGallery(data, options);*/
 
-            //console.log("data fetched from cache");
-            this.renderGallery(data, options);
         } else {
-            this.getShopData(collection, options);
+
+            const hasCachedData = sessionStorage.getItem("shop-data", true);
+
+            if (hasCachedData === "true") {
+                const data = JSON.parse(sessionStorage.getItem("shop"));
+
+                //console.log("data fetched from cache");
+                this.renderGallery(data, options);
+            } else {
+                this.getShopData(collection, options);
+            }
         }
     },
-    getShopOptions(collection) {
+    getShopOptions (collection) {
         const availableToSell = collection.dataset.prices;
 
         const tag = collection.dataset.tag;
@@ -31,16 +43,16 @@ const productListController = {
             availableToSell,
             priceMax,
             priceMin
-        }
+        };
     },
-    getShopData(collection, options) {
+    getShopData (collection, options) {
         const params = {
             format: "json"
         };
 
         const url = collection.dataset.collection;
 
-        axios.get('/' + url, { params })
+        axios.get(`/${ url}`, { params })
             .then((response) => {
                 if (response.data.items.length > 0) {
                     sessionStorage.setItem("shop-data", true);
@@ -48,7 +60,7 @@ const productListController = {
                     this.renderGallery(response.data, options);
                 }
             })
-            .catch(function(error) {
+            .catch((error) => {
                 if (error.response) {
                     // The request was made and the server responded with a status code 
                     // that falls out of the range of 2xx 
@@ -62,18 +74,20 @@ const productListController = {
                     console.log(error.request);
                 } else {
                     // Something happened in setting up the request that triggered an Error 
-                    console.log('Error', error.message);
+                    console.log("Error", error.message);
                 }
                 console.log(error.config);
             });
     },
-    renderGallery(data, options) {
+    renderGallery (data, options) {
         //extend components
-        Vue.component('back-to-top', backToTopButton);
+        Vue.component("back-to-top", backToTopButton);
         Vue.component("image-component", productListImage);
 
         //render the gallery
         const galleryList = new Vue(productList(data, options));
+
+        return galleryList;
     }
 };
 
